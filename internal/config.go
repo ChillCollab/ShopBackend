6e4554c6637e900bfab6ba3a605c8b5b35d2e069
Daniil Petrov <dpetrov@msoftgroup.ru>
2024-05-04 19:26:16 +0300

    Refactor config.go and logger.go files

--- internal/config.go
+++ internal/config.go
@@ -4,6 +4,8 @@ import (
 	"backend/docs"
 	dataBase "backend/internal/dataBase/models"
 	"backend/internal/routes"
+	"backend/pkg/logger"
+	"os"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -14,13 +16,19 @@ import (
 type app struct {
 	server   *gin.Engine
 	dataBase *gorm.DB
+	logger   logger.Logger
 }
 
 func Run() error {
+
 	app := &app{
 		server:   gin.Default(),
 		dataBase: dataBase.DB,
+		logger:   logger.GetLogger(),
 	}
+
+	app.logger.Info("Server starting...")
+
 	docs.SwaggerInfo.BasePath = "/api_v1"
 	config := cors.DefaultConfig()
 	config.AllowOrigins = []string{"http://127.0.0.1:5173", "http://localhost:5173", "http://127.0.0.1:5173/admin"}
@@ -28,7 +36,7 @@ func Run() error {
 	config.AllowHeaders = []string{"Origin", "Authorization", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Access-Control-Allow-Origin"}
 
 	app.server.Use(cors.New(config))
-	err := godotenv.Load()
+	err := godotenv.Load("../.env")
 	if err != nil {
 		return err
 	}
@@ -38,5 +46,11 @@ func Run() error {
 	}
 
 	routes.Routes(app.server)
+
+	runErr := app.server.Run(":" + os.Getenv("APP_PORT"))
+	if runErr != nil {
+		return runErr
+	}
+
 	return nil
 }
